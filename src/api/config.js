9e1df095948eb644b3c2fba138a990eca2eb4517
Kevin Luna <kevin_2044@hotmail.com>
2017-11-07 11:17:54 -0700

    Initial Commit

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+export default{
+    appname: 'platzimusic',
+    apiKey: 'e21cc6cd48ac7c93a3b21e7f4c17ded9',
+    secret: '52789e820f62f84b4c84d9ad14b8f54a',
+    registeredTo: 'kevin_luna'
+}
+/* Application name	platzimusic
+API key	e21cc6cd48ac7c93a3b21e7f4c17ded9
+Shared secret	52789e820f62f84b4c84d9ad14b8f54a
+Registered to	kevin_luna */
---
+++ src/components/Artist.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    li
+      h2: a(v-bind:href="artist.url" target="_blank") {{ artist.name }}
+      img(v-bind:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style lang="stylus" scoped>/* scoped les agrega un data para que no se pisen los estilos entre si */
+li
+  diplay block
+  margin 10px 0
+</style>
